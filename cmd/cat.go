17007db5fa1b5587bc9409d4bd0d387ecdd23b5c
fmhc <fmh@fmhc.de>
2021-06-06 20:28:46 +0200

    added cat command for integrated testing
    added cat command

---
+++ cmd/cat.go
@@ -0,0 +1,51 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// catCmd represents the config command
+var catCmd = &cobra.Command{
+	Use:   "cat",
+	Short: "creates a cat on the dogechain",
+	Long: `The idea is to send a cat image over the dogechain
+to have a proof of concept on a very basic level.
+
+We want to bring a new protocol addon to the dogechain
+which can coexist within transactions and does not need
+any changes on the existing dogecoin-core.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("CAT CALL")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(catCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// catCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// catCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/dgraph-io/badger/v2 v2.2007.2 // indirect
 	github.com/joho/godotenv v1.3.0 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
+	github.com/spf13/afero v1.1.2 // indirect
 	github.com/spf13/cobra v1.1.3
 	github.com/spf13/viper v1.7.1
 	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
